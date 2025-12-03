5d8ccbe93feaed40e4cda7ca9744f45f23b099df
Creature-Keamon <Shuttlez4@outlook.com>
2025-12-03 21:41:04 +1300

    circle with motion js

--- portfoliosite/src/Nav.tsx
+++ portfoliosite/src/Nav.tsx
@@ -1,4 +1,4 @@
-import { motion, useScroll } from "motion/react";
+import { motion, useScroll, useSpring } from "motion/react";
 
 interface Props {
   Navigate: (item: string) => void;
@@ -6,33 +6,58 @@ interface Props {
 
 function RotatingDisk({ Navigate }: Props) {
   const { scrollY } = useScroll();
+  const turn = useSpring(scrollY, {
+    stiffness: 20,
+  });
 
   return (
     <div className="nav-wrapper">
-      <motion.div className="disk-wrapper" style={{ rotate: scrollY }}>
-      <motion.svg
-        width="100%"
-        height="100%"
-        xmlns="http://www.w3.org/2000/svg"
-        className="scroll-disk"
-      >
-        <defs>
-          <linearGradient id="grad1" x1="0%" x2="100%" y1="0%" y2="0%">
-            <stop offset="0%" stop-color="yellow" />
-            <stop offset="100%" stop-color="red" />
-          </linearGradient>
-        </defs>
-        <motion.circle
-          cx="35vw"
-          cy="50vh"
-          r="35vw"
-          fill="url(#grad1)"
-          style={{ rotate: scrollY }}
-        />
-      </motion.svg>
-    </motion.div>
-  </div>
-  ); 
+      <motion.div className="disk-wrapper" style={{ height: turn }}>
+        <motion.svg
+          height="70vw"
+          width="70vw"
+          xmlns="http://www.w3.org/2000/svg"
+          className="scroll-svg"
+          style={{ rotate: turn }}
+        >
+          <defs>
+            <linearGradient id="grad1" x1="0%" x2="100%" y1="0%" y2="0%">
+              <stop offset="0%" stop-color="yellow" />
+              <stop offset="100%" stop-color="red" />
+            </linearGradient>
+          </defs>
+          <circle
+            className="scroll-circle"
+            cx="35vw"
+            cy="35vw"
+            r="35vw"
+            fill="url(#grad1)"
+          />
+          <circle
+            r="15vw"
+            cx="35vw"
+            cy="52vw"
+            fill="yellow"
+            className="minor-circle"
+          />
+          <circle
+            r="15vw"
+            cx="35vw"
+            cy="52vw"
+            fill="yellow"
+            className="minor-circle"
+          />
+          <circle
+            r="15vw"
+            cx="35vw"
+            cy="52vw"
+            fill="yellow"
+            className="minor-circle"
+          />
+        </motion.svg>
+      </motion.div>
+    </div>
+  );
 }
 
 export default RotatingDisk;
