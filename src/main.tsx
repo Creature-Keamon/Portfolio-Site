f82cceaf92ce9a90c8ea1d356a79c76a6f1d9e07
Creature-Keamon <Shuttlez4@outlook.com>
2026-07-13 11:55:38 +1200

    changing routes again for hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -12,12 +12,12 @@ import Projects from "./data/ProjectList";
 createRoot(document.getElementById("root")!).render(
   <HashRouter>
     <Routes>
-      <Route path="/Portfolio-Site/" element={<RotatingDisk />} />
-      <Route path="/Portfolio-Site/Projects" element={<ProjectsPage />} />
-      <Route path="/Portfolio-Site/Contact" element={<Contact />} />
-      <Route path="/Portfolio-Site/About" element={<About />} />
+      <Route path="/" element={<RotatingDisk />} />
+      <Route path="/Projects" element={<ProjectsPage />} />
+      <Route path="/Contact" element={<Contact />} />
+      <Route path="/About" element={<About />} />
       {Projects.map((Project) => {
-        return (<Route path={"/Portfolio-Site/Projects/" + Project.PageURL} element={<SingleProject data={Project.data} />} />)
+        return (<Route path={"/Projects/" + Project.PageURL} element={<SingleProject data={Project.data} />} />)
       })}
     </Routes>
   </HashRouter>
